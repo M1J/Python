7e399a5c9b98b6af3934cf80819c42eba16f80ae
Marek Juhanson <mjuhanso@klass3-02-l.intra.itcollege.ee>
2017-02-02 11:51:00 +0200

    1

---
+++ task3.py
@@ -0,0 +1,17 @@
+class Car: # define the class with a name 
+	
+	
+	def __init__(self, name, wheels=4): #self==c, define a method
+		self.name = name	# assigns a property to the instance
+		self.wheels = wheels
+	def __str__(self):
+		"""
+		give the instance a name. Is called during 'print(intsance)'
+		"""		
+		return "{0} wiht {1} wheels".format(self.name, self.wheels)
+
+	
+	i = 'alo'
+i = Car('Wagon') #make an object from class, ie Instantiation
+print(i) #when print a instance, the __str__method is called
+#c.print_wheels()
